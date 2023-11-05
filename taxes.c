7adfcb6e44c7bd378f8c3952740d6a9e178a2569
Alisha-Batool <144157470+Alisha-Batool@users.noreply.github.com>
2023-11-05 14:54:46 -0800

    change

--- taxes.c
+++ taxes.c
@@ -27,23 +27,31 @@ int main(int argc, char **argv) {
     printf("How many children do you have? ");
     scanf("%d", &numChildren);
   }
-   if (agi <= 20000) {
+   if (agi <= 19900)
+  {
     tax = agi * 0.1;
-  } else if (agi <= 50000) {
-    tax = 2000 + (agi - 20000) * 0.15;
-  } else if (agi <= 100000) {
-    tax = 7000 + (agi - 50000) * 0.2;
-  } else {
-    tax = 17000 + (agi - 100000) * 0.25;
   }
-
+   else if (agi <= 81050) 
+  {
+    tax = 2000 + (agi - 19900) * 0.15;
+  }
+   else if (agi <= 172750) 
+  {
+    tax = 7000 + (agi - 81050) * 0.2;
+  }
+   else 
+ {
+    tax = 17000 + (agi - 172750) * 0.25;
+  
+ }
   // Compute the child credit based on the number of children
   childCredit = numChildren * 1000;
 
   // Compute the total tax
   totalTax = tax - childCredit;
   
-
+  //display result
+  printf("\nTax Calculation:\n");
   printf("AGI:          $%10.2f\n", agi);
   printf("Tax:          $%10.2f\n", tax);
   printf("Child Credit: $%10.2f\n", childCredit);
