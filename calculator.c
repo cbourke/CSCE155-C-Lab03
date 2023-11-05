0a247d920cca3ab91a1611b896e8244ffbd688b7
Alisha-Batool <144157470+Alisha-Batool@users.noreply.github.com>
2023-11-05 14:25:54 -0800

    write code in function

--- calculator.c
+++ calculator.c
@@ -10,6 +10,63 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
+double calculator( int choice, double a, double b );
+double calculator( int choice, double a, double b ){
+  double result= 0.0;
+    if(choice == 1)
+   {
+    result = a + b;
+      printf("Result: %.2f\n", result);
+    
+  }
+   else if(choice == 2) 
+  {
+    result = a - b;
+    printf("%.2f", result);
+    
+  }
+   else if(choice == 3)
+  {
+    result = a * b;
+      printf("Result: %.2f\n", result);
+    
+    
+  } //TODO: handle this case (division)
+  else if(choice == 4) 
+  {
+    if (b != 0) {
+        result = a / b;
+        printf("Result: %.2f\n", result);
+        
+      } else {
+        printf("Cannot divide by zero.\n");
+      }
+
+    
+  }//TODO: handle this case (minimum)
+   else if(choice == 5) 
+  {
+    result = fmin(a, b);
+      printf("Result: %.2f\n", result);
+    
+    
+  }//TODO: handle this case (log_a(b))
+   else if(choice == 6) 
+  {
+    if (a > 0 && a != 1) {
+        result = log(b) / log(a);
+        printf("Result: %.2f\n", result);
+      } else {
+        printf("Invalid base for logarithm.\n");
+      }
+    
+  } 
+  else
+  {
+    printf("Please input a valid operator next time");
+  }
+  return result;
+}
 
 int main(int argc, char **argv) {
 
@@ -30,45 +87,9 @@ int main(int argc, char **argv) {
   printf("(5) Minimum\n");
   printf("(6) log_a(b)\n");
   scanf("%d", &choice);
-
-  if(choice == 1) {
-    result = a + b;
-      printf("Result: %.2f\n", result);
-    
-  } else if(choice == 2) {
-    result = a - b;
-    printf("%.2f", result);
-  } else if(choice == 3) {
-    result = a * b;
-      printf("Result: %.2f\n", result);
-    
-    
-  } else if(choice == 4) {
-    if (b != 0) {
-        result = a / b;
-        printf("Result: %.2f\n", result);
-      } else {
-        printf("Cannot divide by zero.\n");
-      }
-    
-    
-    //TODO: handle this case (division)
-  } else if(choice == 5) {
-    result = fmin(a, b);
-      printf("Result: %.2f\n", result);
-    
-    //TODO: handle this case (minimum)
-  } else if(choice == 6) {
-    if (a > 0 && a != 1) {
-        result = log(b) / log(a);
-        printf("Result: %.2f\n", result);
-      } else {
-        printf("Invalid base for logarithm.\n");
-      }
-    //TODO: handle this case (log_a(b))
-  } else {
-    printf("Please input a valid operator next time");
-  }
-
+  result= calculator(choice,  a, b);
+  printf("Result: %.2f\n", result);
+ 
+  
   return 0;
 }
